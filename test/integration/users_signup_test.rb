e07ef4dfdf8aba20c079fbe067343b875b97c0ca
Sonny Godwin <srgodwin3@yahoo.com>
2015-12-16 17:18:30 +0000

    Use SSL and the PUma webserver in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,7 +14,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   ## Test: Enter valid informaiton and check to see if user was added to database
   test "valid signup information" do
     get signup_path
-    assert_diffference 'User.count', 1 do
+    assert_difference 'User.count', 1 do
       post_via_redirect users_path,  user: { name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password" }
     end
     assert_template "users/show"
